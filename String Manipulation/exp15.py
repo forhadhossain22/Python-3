5d3cca7558a8518268266fe2de986a9b15f794c0
Forhad Hossain <104172772+forhadhossain22@users.noreply.github.com>
2022-11-21 13:02:12 +0600

    Add files via upload

---
+++ String Manipulation/exp15.py
@@ -0,0 +1,11 @@
+sentence = 'How can a clam cram in a clean cream can?'
+
+print(sentence)
+
+print(sentence.replace('c','d'))
+
+print(sentence.replace('?',''))
+
+print(sentence.strip('?'))
+
+print(sentence.split(' '))
