bfc6939e0f08a9d9db5af108e083c06fcd8f8dc3
Forhad Hossain <104172772+forhadhossain22@users.noreply.github.com>
2022-11-21 12:53:22 +0600

    Add files via upload

---
+++ String Manipulation/exp13.py
@@ -0,0 +1,8 @@
+sentence = 'How can a clam cram in a clean cream can?'
+
+print(sentence.count('c'))
+print(sentence.count('c',5))
+print(sentence.count('c',5,9))
+
+print(sentence.find('c'))
+print(sentence.find('c',5))
---
+++ String Manipulation/exp14.py
@@ -0,0 +1,7 @@
+
+
+a = 'bangladesh is my country'
+
+print(a.find('x'))
+
+print(a.index('x'))
